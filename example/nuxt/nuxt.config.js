290c143dc75775f103b6f265926814d723005e0d
Stuart Clark <stu@rtclark.net>
2022-04-11 06:15:38 +0000

    chore(#25): update nuxt config

--- example/nuxt/nuxt.config.js
+++ example/nuxt/nuxt.config.js
@@ -1,9 +1,11 @@
+const baseUrl = process.env.GITPOD_WORKSPACE_ID
+  ? `https://8080-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
+  : 'http://druxt-module-template.ddev.site'
+
 export default {
   buildModules: [
     'druxt',
     'druxt-module-template'
   ],
-  druxt: {
-    baseUrl: 'https://demo-api.druxtjs.org'
-  }
+  druxt: { baseUrl }
 }
